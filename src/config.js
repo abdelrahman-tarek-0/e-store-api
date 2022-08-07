29aa98ce9c581b0bb64c6018c02a6a3db395836a
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2022-08-07 19:26:32 +0200

    update: handling env vars

--- src/config.js
+++ src/config.js
@@ -3,6 +3,12 @@ dotenv.config()
 
 module.exports = {
    port: process.env.PORT || 3000,
-   url: process.env.URL || 'http://localhost:3000',
    env: process.env.NODE_ENV || 'development',
+   db_url: process.env.DB_URL || null,
+   db_host: process.env.DB_HOST ,
+   db_port: process.env.DB_PORT ,
+   db_name: process.env.DB_NAME ,
+   db_test: process.env.DB_TEST ,
+   db_user: process.env.DB_USER ,
+   db_pass: process.env.DB_PASS 
 }
