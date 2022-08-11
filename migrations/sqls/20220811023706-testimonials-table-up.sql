63b310f3e0cef5c3ceadb3ff7cc731f33f6c9be5
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2022-08-11 04:41:19 +0200

    update: create table for testimonials

--- migrations/sqls/20220811023706-testimonials-table-up.sql
+++ migrations/sqls/20220811023706-testimonials-table-up.sql
@@ -1,1 +1,7 @@
-/* Replace with your SQL commands */
+create table testimonials(
+  id SERIAL PRIMARY KEY,
+  name varchar(255) not null unique,
+  testimonial text not null,
+  image text not null,
+  created_at timestamp not null default now()
+);
