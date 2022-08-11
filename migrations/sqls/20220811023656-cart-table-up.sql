ccc1429a61b4353006f2a1bbf818ea3902e18201
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2022-08-11 04:40:40 +0200

    update: create table for cart

--- migrations/sqls/20220811023656-cart-table-up.sql
+++ migrations/sqls/20220811023656-cart-table-up.sql
@@ -1,1 +1,6 @@
-/* Replace with your SQL commands */
+create table cart(
+  id SERIAL PRIMARY KEY,
+  user_id TEXT NOT NULL,
+  items JSONB NOT NULL,
+  created_at timestamp not null default now()
+);
